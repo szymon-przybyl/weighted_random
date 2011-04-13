62f7e3e08b0e3b2ab1611464e3a367f3392aa419
Szymon Przybył <apocalyptiq@gmail.com>
2011-04-13 21:33:58 +0200

    RECORDS - amount of test records

--- examples/performance.rb
+++ examples/performance.rb
@@ -1,4 +1,5 @@
 TIMES = (ENV['N'] || 1000).to_i
+RECORDS = 1000
 
 require 'active_record'
 require 'active_support'
@@ -25,21 +26,19 @@ end
 require 'benchmark'
 
 HASHES = []
-TIMES.times { |i| HASHES[i] = { :name => 'Sample name', :weight => rand(1000) } }
+RECORDS.times { |i| HASHES[i] = { :name => 'Sample name', :weight => rand(1000) } }
 
 Benchmark.bmbm do |bm|
-  RECORDS = TIMES/10
-
   bm.report 'WRModel.create for one record on empty table' do
     TIMES.times do |i|
-      Exhibit.create HASHES[i]
+      Exhibit.create HASHES[i % RECORDS]
       Exhibit.delete_all
     end
   end
 
   bm.report "WRModel.create for #{RECORDS} records on empty table" do
     (TIMES/RECORDS).times do |i|
-      Exhibit.create HASHES[i*RECORDS, RECORDS]
+      Exhibit.create HASHES
       Exhibit.delete_all
     end
   end
@@ -47,20 +46,20 @@ Benchmark.bmbm do |bm|
   Exhibit.create HASHES
 
   bm.report "WRModel.create for one record on table with #{TIMES}+ records" do
-    TIMES.times { |i| Exhibit.create HASHES[i] }
+    TIMES.times { |i| Exhibit.create HASHES[i % RECORDS] }
   end
 
   Exhibit.delete_all
   Exhibit.create HASHES
 
   bm.report "WRModel.create for #{RECORDS} records on table with #{TIMES}+ records" do
-    (TIMES/RECORDS).times { |i| Exhibit.create HASHES[i*RECORDS, RECORDS] }
+    (TIMES/RECORDS).times { |i| Exhibit.create HASHES }
   end
 
   Exhibit.delete_all
-  Exhibit.create HASHES
+  10.times { Exhibit.create HASHES }
 
-  bm.report "WRModel.weighted_rand on table with #{TIMES} records" do
+  bm.report "WRModel.weighted_rand on table with #{TIMES*10} records" do
     TIMES.times { Exhibit.weighted_rand }
   end
 
